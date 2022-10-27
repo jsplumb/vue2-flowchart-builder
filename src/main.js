27221e42e7b68ccc0841fa9bfc12b6ecff7ae854
Simon Porritt <simon@jsplumbtoolkit.com>
2022-10-27 11:51:31 +1100

    doc update

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { JsPlumbToolkitVue2Plugin } from '@jsplumbtoolkit/browser-ui-vue2'
 
 Vue.config.productionTip = false
 
-// import plugin the new way
+// import Toolkit plugin
 Vue.use(JsPlumbToolkitVue2Plugin)
 
 new Vue({ render: h => h(App) }).$mount('#app')
